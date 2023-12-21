98797dc89a659d07495d6b685a537099ee7af064
Smit Mistry <84773605+smit-lab@users.noreply.github.com>
2023-12-21 10:48:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const bodyColor = document.querySelector("body");
+const changeText = document.querySelector("#clicked-color");
+const notShow = document.querySelector("#not-show");
+const submitButton = document.querySelector("#submit-btn");
+let customColorInput = document.querySelector("#color-name");
+
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  let inputValue = customColorInput.value;
+  if (inputValue !== "") {
+    bodyColor.style.backgroundColor = inputValue;
+    changeText.textContent = customColorInput.value;
+    notShow.style.display = "none";
+    console.log(`You've typed: ${inputValue} color`);
+  }
+});
